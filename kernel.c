fefa90420f037ec4b87654955f4b340affce8b91
pujan-p <pujanp@uw.edu>
2025-08-17 23:12:25 -0700

    printing the dtb

--- kernel.c
+++ kernel.c
@@ -1,18 +1,18 @@
 #include <stdint.h>
 
+extern uint8_t _dtb_start;
 volatile uint8_t *uart = (uint8_t *)0x09000000;
 
-void putChar(char c) { *uart = c; }
-
-void print(const char *str) {
-  while (*str != '\n') {
-    putChar(*str);
-    str++;
-  }
-}
+void uart_putc(char c) { *uart = c; }
 
 void kernel(void) {
-  print("Hello World");
+  uint8_t *dtb = &_dtb_start;
+
+  while (*dtb != '\n') {
+    uart_putc(*dtb);
+    dtb++;
+  }
+
   while (1) {
   }
 }
